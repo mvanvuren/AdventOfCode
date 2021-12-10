d7557d9e8453addaeabc8bfcfda0cee133cbcb3d
mvanvuren <m.vuren@upcmail.nl>
2021-12-10 07:33:57 +0100

    S9

---
+++ 2021/Aoc.S10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Aoc.S10
+{
+    class Program
+    {
+        static void Main()
+        {
+            var parser = new Stack<char>();
+            ulong score = 0;
+            var points = new Dictionary<char, ulong> { { ')', 3 }, { ']', 57 }, { '}', 1197 }, { '>', 25137 } };
+            var match = new Dictionary<char, char> { { '(', ')' }, { '[', ']' }, { '{', '}' }, { '<', '>' } };
+            foreach (var line in File.ReadAllLines("data.txt"))
+            {
+                foreach (var c in line)
+                {
+                    if (c is ')' or ']' or '}' or '>')
+                    {
+                        var mc = parser.Peek();
+                        if (c != mc)
+                        {
+                            Console.WriteLine($"{line} - Expected {mc}, but found {c} instead.");
+                            score += points[c];
+                            parser.Clear();
+                            break;
+                        }
+                        parser.Pop();
+                    }
+                    else
+                    {
+                        parser.Push(match[c]);
+                    }
+                }
+            }
+            Console.WriteLine(score); // S10.1: 294195
+        }
+    }
+}
---
+++ 2021/Aoc.S9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Aoc.S9
+{
+    class Program
+    {
+        static void Main()
+        {
+            var board = File.ReadAllLines("data.txt").Select(l => l.Select(c => (byte)(c - '0')).ToArray())
+                .ToArray();
+            ulong riskLevel = 0;
+            for (var x = 0; x < board.Length; x++)
+            {
+                for (var y = 0; y < board[x].Length; y++)
+                {
+                    riskLevel += CalculateRiskLevel(board, x, y);
+                }
+            }
+            Console.WriteLine(riskLevel); // S9.1: 439
+        }
+
+        static byte CalculateRiskLevel(IReadOnlyList<byte[]> board, int x, int y)
+        {
+            var p = board[x][y];
+            
+            if ((x > 0 && board[x - 1][y] <= p)
+                || (x < board.Count - 1 && board[x + 1][y] <= p)
+                || (y > 0 && board[x][y - 1] <= p)
+                || (y < board[x].Length - 1 && board[x][y + 1] <= p)) return 0;
+
+            return (byte)(p + 1);
+        }
+    }
+}
